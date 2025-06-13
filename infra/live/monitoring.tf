1cd85545745f122278ff86a01cfeccf9ab501330
middleKD <kmiddlecenter@gmail.com>
2025-06-13 19:48:49 +0900

    update: prometheus and AMP with grafana

--- infra/live/monitoring.tf
+++ infra/live/monitoring.tf
@@ -26,10 +26,10 @@ module "loki_irsa" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "5.39.0"
 
-  create_role           = true
-  role_name             = "loki-irsa-role"
-  provider_url          = module.eks.oidc_provider
-  role_policy_arns      = [aws_iam_policy.live_s3_rw_policy.arn]
+  create_role      = true
+  role_name        = "loki-irsa-role"
+  provider_url     = module.eks.oidc_provider
+  role_policy_arns = [aws_iam_policy.live_s3_rw_policy.arn]
   oidc_fully_qualified_subjects = [
     "system:serviceaccount:monitoring:loki-s3-sa",
   ]
@@ -47,7 +47,7 @@ resource "helm_release" "loki_stack" {
 
   set {
     name  = "loki.config.storage_config.aws.s3"
-    value = "${aws_s3_bucket.loki_s3_storage.bucket}"
+    value = aws_s3_bucket.loki_s3_storage.bucket
   }
 
   set {
@@ -64,4 +64,87 @@ resource "helm_release" "loki_stack" {
     name  = "loki.serviceAccount.name"
     value = kubernetes_service_account.loki_s3_sa.metadata[0].name
   }
+
+  set {
+    name  = "grafana.plugins[0]"
+    value = "grafana-amazonprometheus-datasource"
+  }
+}
+
+resource "helm_release" "prometheus_stack" {
+  name       = "prometheus-stack"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  namespace  = "monitoring"
+
+  set {
+    name  = "grafana.enabled"
+    value = "false"
+  }
+
+  set {
+    name  = "prometheus.serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "prometheus.serviceAccount.name"
+    value = kubernetes_service_account.amp_sa.metadata[0].name
+  }
+
+  set {
+    name  = "prometheus.prometheusSpec.remoteWrite[0].url"
+    value = "https://aps-workspaces.ap-northeast-2.amazonaws.com/workspaces/${aws_prometheus_workspace.live_amp_workspace.id}/api/v1/remote_write"
+  }
+
+  set {
+    name  = "prometheus.prometheusSpec.remoteWrite[0].sigv4.region"
+    value = var.region
+  }
+}
+
+resource "aws_prometheus_workspace" "live_amp_workspace" {
+  alias = "live-amp-workspace"
+}
+
+resource "aws_iam_policy" "amp_remote_write" {
+  name = "AMPRemoteWritePolicy"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "aps:RemoteWrite",
+          "aps:GetSeries",
+          "aps:GetLabels",
+          "aps:GetMetricMetadata"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "kubernetes_service_account" "amp_sa" {
+  metadata {
+    name      = "amp-sa"
+    namespace = "monitoring"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = module.amp_irsa.iam_role_arn
+    }
+  }
+}
+
+module "amp_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "5.39.0"
+
+  create_role      = true
+  role_name        = "amp-irsa-role"
+  provider_url     = module.eks.oidc_provider
+  role_policy_arns = [aws_iam_policy.amp_remote_write.arn]
+  oidc_fully_qualified_subjects = [
+    "system:serviceaccount:monitoring:amp-sa",
+  ]
 }
