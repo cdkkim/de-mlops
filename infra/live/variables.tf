0ab100237188c9771f18f5f6e4863fd89ac0223d
middleKD <kmiddlecenter@gmail.com>
2025-06-18 21:16:26 +0900

    update: user feature

--- infra/live/variables.tf
+++ infra/live/variables.tf
@@ -7,5 +7,17 @@ variable "region" {
 variable "profile" {
   description = "AWS SSO profile"
   type        = string
-  default     = "middlek"
+  default     = "middlek-eks"
+}
+
+variable "sso_admin_role_arn" {
+  description = "SSO admin role ARN"
+  type        = string
+  default = "arn:aws:iam::456330605094:role/AWSReservedSSO_AdministratorAccessEKSAdmin_55ddd04e054845b8"
+}
+
+variable "sso_developer_role_arn" {
+  description = "SSO developer role ARN"
+  type        = string
+  default = "arn:aws:iam::456330605094:role/AWSReservedSSO_AdministratorAccess_55ddd04e054845b8"
 }
