6dafad75720358bae629d2150f2494e09354a7e0
middleKD <kmiddlecenter@gmail.com>
2025-07-05 20:57:34 +0900

    fix: wron folder structures

--- src/src/components/LoadingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingSpinner() {
-  return (
-    <div className="flex justify-center items-center h-full w-full">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
-    </div>
-  );
-} 
